afc2ab1fc5d3e73b7d242ac15fb9af1b33c6a609
tacchan7412 <tacchan.04@gmail.com>
2018-05-02 21:00:03 +0900

    up to v1.0.4

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python_slack_print',
-    version='1.0.0',
+    version='1.0.4',
     description='Python print for stdout and slack',
     long_description=long_description,
     url='https://github.com/tacchan7412/python_slack_print',
     author='Tatsuki Koga',
     author_email='tacchan.04@gmail.com',
     license='MIT',
-    packages=find_packages('slack_print'),
+    packages=find_packages(),
     install_requires=['slacker']
 )
